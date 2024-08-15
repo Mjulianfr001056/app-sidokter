aff1fc2abf1b43aea26360f678eb844755868f51
Mjulianfr001056 <222112217@stis.ac.id>
2024-08-15 10:39:13 +0700

    create dashboard v1.0

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
+    @livewireStyles
     <title>@yield('title')</title>
 </head>
 <body>
@@ -16,24 +17,11 @@
     </div>
     <div class="drawer-side">
         <ul class="menu text-gray-50 min-h-full w-80 p-4 bg-teal-600">
-            <x-navigation.link
-                route="{{ route('dashboard') }}"
-                :active="request()->routeIs('dashboard')"
-                text="Dashboard"
-            />
-            <x-navigation.link
-                route="{{ route('kegiatan') }}"
-                :active="request()->routeIs('kegiatan')"
-                text="Kegiatan"
-            />
-            <x-navigation.link
-                route="{{ route('beban-kerja') }}"
-                :active="request()->routeIs('beban-kerja')"
-                text="beban-kerja"
-            />
+            <livewire:sidebar-links />
         </ul>
     </div>
 </div>
 
+@livewireScripts
 </body>
 </html>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,5 +3,118 @@
 @section('title', 'Dashboard')
 
 @section('content')
-    <h1>Welcome to your dashboard</h1>
+    <div class="size-full bg-gray-50 flex flex-col w-full items-center">
+        <h1 class="text-4xl font-bold text-teal-600 mb-8">Dashboard</h1>
+        <div class="grid grid-cols-2 size-full">
+            {{--                Kolom pertama--}}
+            <div class="flex flex-col space-y-4">
+                {{--                Periode--}}
+                <div class="dropdown dropdown-bottom pl-4">
+                    <div tabindex="0" role="button"
+                         class="btn m-2 bg-teal-700 border-0 text-gray-50 justify-between self-center">
+                        <div class="self-center pr-8">
+                            <p class="text-md text-gray-50">Periode</p>
+                        </div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                             stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5"/>
+                        </svg>
+                    </div>
+                    <ul tabindex="0" class="dropdown-content menu bg-gray-50 z-[1] w-52 p-2 shadow">
+                        <li><a class="text-cyan-950">Item 1</a></li>
+                        <li><a class="text-cyan-950">Item 2</a></li>
+                        <li><a class="text-cyan-950">Item 3</a></li>
+                        <li><a class="text-cyan-950">Item 4</a></li>
+                    </ul>
+                </div>
+
+                {{--                Kartu--}}
+                <div class="w-full flex flex-row justify-around px-2">
+                    {{--                Kartu pertama--}}
+                    <div class="card bg-teal-600 w-full mx-4 shadow-xl rounded-t-md">
+                        <div class="card-body py-2 px-4">
+                            <p class="card-title text-gray-50 text-center text-lg px-4">Jumlah kegiatan periode ini</p>
+                        </div>
+                        <div>
+                            <div class="flex justify-center bg-gray-50 h-24 rounded-b-md">
+                                <p class="text-5xl text-teal-700 font-bold self-center pb-2">10</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    {{--                    Kartu kedua--}}
+                    <div class="card bg-teal-600 w-full mx-4 shadow-xl rounded-t-md">
+                        <div class="card-body py-2 px-4">
+                            <p class="card-title text-gray-50 text-center text-lg px-4 ">Rata-rata beban organik</p>
+                        </div>
+                        <div>
+                            <div class="flex justify-center bg-gray-50 h-24 rounded-b-md">
+                                <p class="text-5xl text-teal-700 font-bold self-center pb-2">5</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {{--                Grafik--}}
+                <div class="size-full px-4">
+                    <div class="card w-full shadow-xl rounded-none">
+                        <div class="card-body py-2 px-4">
+                            <p class="card-title text-teal-700 text-center text-xl px-4 font-bold self-center">Beban
+                                kerja organik periode
+                                ini</p>
+                        </div>
+
+                        {{--                        Grafik beban--}}
+                        <div class="p-4">
+                            <div style="min-height:423px" id="datawrapper-vis-9Dxft">
+                                <script type="text/javascript" defer src="https://datawrapper.dwcdn.net/9Dxft/embed.js"
+                                        charset="utf-8" data-target="#datawrapper-vis-9Dxft"></script>
+                                <noscript><img src="https://datawrapper.dwcdn.net/9Dxft/full.png" alt=""/></noscript>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {{--            Kolom Kedua--}}
+            <div class="flex flex-col mb-5 mr-2 space-y-8">
+                <div class="size-full px-4">
+                    <div class="card w-full shadow-xl rounded-none">
+                        <div class="card-body py-2 px-4">
+                            <p class="card-title text-teal-700 text-center text-xl px-4 font-bold self-center">Kegiatan
+                                setiap fungsi</p>
+                        </div>
+
+                        {{--                        Grafik kegiatan--}}
+                        <div class="p-4">
+                            <div style="min-height:468px" id="datawrapper-vis-ShNFa">
+                                <script type="text/javascript" defer src="https://datawrapper.dwcdn.net/ShNFa/embed.js"
+                                        charset="utf-8" data-target="#datawrapper-vis-ShNFa"></script>
+                                <noscript><img src="https://datawrapper.dwcdn.net/ShNFa/full.png"
+                                               alt="A visualization of a pie chart called &quot;What do journalists drink?&quot; Compiled from a poll Kit O'Connell of the Texas Observer did on Mastodon. It's &quot;very scientific&quot; haha not really. Results: 49% coffee, 23% tea, 5% energy drinks and 23% spite. Based on 357 responses. &quot;Spite&quot; option was also the &quot;Show results&quot; choice which makes this even less useful."/>
+                                </noscript>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="size-full px-4">
+                    <div class="card w-full shadow-xl rounded-none">
+                        <div class="card-body py-2 px-4">
+                            <p class="card-title text-teal-700 text-center text-xl px-4 font-bold self-center">Kegiatan
+                                setiap periode</p>
+                        </div>
+
+                        {{--                        Grafik kegiatan periode--}}
+                        <div class="p-4">
+                            <div style="min-height:432px" id="datawrapper-vis-ZPqob">
+                                <script type="text/javascript" defer src="https://datawrapper.dwcdn.net/ZPqob/embed.js"
+                                        charset="utf-8" data-target="#datawrapper-vis-ZPqob"></script>
+                                <noscript><img src="https://datawrapper.dwcdn.net/ZPqob/full.png" alt=""/></noscript>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
