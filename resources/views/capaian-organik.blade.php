d08596e70d6788987f0da4af636d7f57a63ec6cd
Mjulianfr001056 <222112217@stis.ac.id>
2024-08-22 08:27:25 +0700

    Capaian v1.0

--- resources/views/capaian-organik.blade.php
+++ resources/views/capaian-organik.blade.php
@@ -46,15 +46,62 @@
                             <x-selesai-badge/>
                         </td>
                         <td>
-                            <button class="flex items-center justify-center bg-blue-700 text-gray-50 hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-500 rounded-lg w-20 py-0.5 text-sm font-medium">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-2">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                                </svg>
-                                View
-                            </button>
+                            <x-view-button/>
                         </td>
                     </tr>
+                    <tr>
+                        <td>2</td>
+                        <td class="date-cell">2021-10-01</td>
+                        <td>Survei Biaya Hidup</td>
+                        <td>John Doe</td>
+                        <td>Pengawas</td>
+                        <td>
+                            <x-selesai-badge/>
+                        </td>
+                        <td>
+                            <x-view-button/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td class="date-cell">2021-10-01</td>
+                        <td>Survei Biaya Hidup</td>
+                        <td>John Doe</td>
+                        <td>Pengawas</td>
+                        <td>
+                            <x-proses-badge/>
+                        </td>
+                        <td>
+                            <x-view-button/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td class="date-cell">2021-10-01</td>
+                        <td>Survei Biaya Hidup</td>
+                        <td>John Doe</td>
+                        <td>Pengawas</td>
+                        <td>
+                            <x-proses-badge/>
+                        </td>
+                        <td>
+                            <x-view-button/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>5</td>
+                        <td class="date-cell">2021-10-01</td>
+                        <td>Survei Biaya Hidup</td>
+                        <td>John Doe</td>
+                        <td>Pengawas</td>
+                        <td>
+                            <x-selesai-badge/>
+                        </td>
+                        <td>
+                            <x-view-button/>
+                        </td>
+                    </tr>
+
                     </tbody>
                 </table>
             </div>
